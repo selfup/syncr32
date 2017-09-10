0542c730fdfb915009a158fc3aec996de77bc8d4
Regis <boudinot.regis@yahoo.com>
2017-09-10 11:48:45 -0400

    make test actually assert something :joy:

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,10 @@ fn integration_test() {
     let actions = Actions::new();
     actions.frontload_root_dirs(&mut state);
     actions.grab_path_meta_data(&mut state);
+
+    assert_eq!(String::from("./src/fixtures"),
+               String::from(state.root_path));
+
+    assert_eq!(std::path::Path::new("./src/fixtures/test.md"),
+               state.directories[0]);
 }
